8df0352a29a6c2dec1970b9a0893fa849bad43a5
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:44:24 +0200

    Update Main.java
    In Ukrainian

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,40 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {//обьявляем публичный класс Главный
-    public static void main(String[] args) throws InterruptedException {//обьявляем главный метод, кот.пробрасывает исключение класса ПрерванноеИсключение (из-за метода калькуляторФинишировавшихЛошадей)
-        List<Horse> horses = prepareHorsesAndStart(10);//обьявляем ссыл.перем. типа интерфейса List<Horse> = методу подготовитьЛошадейИНачать(аргумент: число лошадей)
-        while (calculateHorsesFinished(horses) != horses.size()){//цикл пока (метод калькуляторФинишировавшихЛошадей(аргумент: список horses) НЕ= размеру списка horses)
-            System.out.println("All horses finished!");//печатаем - "Все лошади финишировали!"
+public class Main { // оголошуємо публічний головний клас
+    public static void main(String[] args) throws InterruptedException { // оголошуємо головний метод, який пробрасыває виняток InterruptedException (через метод calculateHorsesFinished)
+        List<Horse> horses = prepareHorsesAndStart(10); // оголошуємо змінну типу інтерфейс List<Horse> і викликаємо метод prepareHorsesAndStart з параметром: кількість коней
+        while (calculateHorsesFinished(horses) != horses.size()) { // цикл поки (метод calculateHorsesFinished з аргументом: список horses) не дорівнює розміру списку horses
+            System.out.println("All horses finished!"); // виводимо "Усі коні фінішували!"
         }
     }
 
-    public static List<Horse> prepareHorsesAndStart(int horsesCount) {//обьявляем публичный статический типа интерфейса List<Horse> метод подготовитьЛошадейИНачать(параметр: число)
-        List<Horse> horses = new ArrayList<>(horsesCount);//обьявляем ссыл.перем.типа интерфейс List<Horse> на новый обьект с поведением класса ArrayList<>(предварительный размер списка: перем.-параметр метода)
-        String number;//обьявляем пустую переменную типа String
+    public static List<Horse> prepareHorsesAndStart(int horsesCount) { // оголошуємо публічний статичний метод типу List<Horse> підготовитиКонейІЗапустити з параметром: число
+        List<Horse> horses = new ArrayList<>(horsesCount); // оголошуємо змінну типу List<Horse> як новий об'єкт з класу ArrayList з попереднім розміром списку
+        String number; // оголошуємо змінну типу String
 
-        for (int i = 1; i < horsesCount + 1; i++) {//обьявляем цикл размером от 1 до перем.-параметр метода + 1 (чтоб счет начинался от единицы) по возрастанию
-            number = i < 10 ? ("0" + i) : "" + i;//перем. number присваиваем значение = (если i < 10 ТО == ("0" + i) ИНАЧЕ ("" + i (пустая строка + индекс цикла))
-            horses.add(new Horse("Horse_" + number));//заполняем список horses (новыми обьектами класса Лошадь(аргумент для конструктора: литерал + переменная number)
+        for (int i = 1; i < horsesCount + 1; i++) { // цикл від 1 до параметра методу + 1 (щоб індексація починалася з 1)
+            number = i < 10 ? ("0" + i) : "" + i; // змінній number присвоюємо значення: якщо i < 10, то "0" + i, інакше просто i
+            horses.add(new Horse("Horse_" + number)); // додаємо новий об'єкт класу Horse в список horses
         }
 
-        System.out.println("All horses start the race!");//печатаем - литерал
-        for (int i = 0; i < horsesCount; i++) {//обьявляем цикл размером от 0 до перем.-параметр метода по возрастанию
-            horses.get(i).start();//ссыл.перем. на список horses.получитьЗначение(по индексу).запуститьПоток()   ------------- ТУТ ЗАПУСКАЕМ ПОТОК
+        System.out.println("All horses start the race!"); // виводимо "Усі коні стартують в гонці!"
+        for (int i = 0; i < horsesCount; i++) { // цикл від 0 до параметра методу по збільшенню
+            horses.get(i).start(); // запускаємо потік для кожного коня
         }
-        return horses;//вернуть содержимое ссыл.перем. на список
+        return horses; // повертаємо список коней
     }
 
-    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException {//обьявляем публичный статический типа int метод калькуляторФинишировавшихЛошадей(параметр: ссыл.перем.типа интерфейс List<Horse>)
-        // который пробрасывает исключение класса ПрерванноеИсключение
-        int finishedCount = 0;//обьявляем переменную типа int счетчикФинишировавших = 0
-        for (int i = 0; i < horses.size(); i++) {//обьявляем цикл размером от 0 до размер списка horses по возрастанию
-            if (horses.get(i).isFinished()) {//если (элемент из списка(по индексу) финишировал(булевая переменная класса Лошадь), то
-                finishedCount++;//переменную счетчикФинишировавших увеличиваем на 1
-            } else {//иначе
-                System.out.println("Waiting for " + horses.get(i).getName());//печатаем - литерал + данный элемент списка horses .получитьИмя()
-                horses.get(i).join();//данный элемент списка horses.вызывает метод join(), кот.заставляет главный поток обождать пока выполниться второстепенный поток
+    public static int calculateHorsesFinished(List<Horse> horses) throws InterruptedException { // публічний статичний метод для обчислення кількості коней, які фінішували
+        int finishedCount = 0; // оголошуємо змінну для підрахунку кількості фінішованих коней
+        for (int i = 0; i < horses.size(); i++) { // цикл від 0 до розміру списку horses
+            if (horses.get(i).isFinished()) { // якщо кінь фінішував
+                finishedCount++; // збільшуємо лічильник фінішованих
+            } else { // інакше
+                System.out.println("Waiting for " + horses.get(i).getName()); // виводимо: "Очікуємо на " + ім'я коня
+                horses.get(i).join(); // чекаємо завершення потоку цього коня
             }
         }
-        return finishedCount;//вернуть счетчикФинишировавших
+        return finishedCount; // повертаємо кількість фінішованих коней
     }
 }
